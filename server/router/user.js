b3b041bd02bc4b5b89d4a598fd2ce51727eb2496
dbstn1325 <78465062+dbstn1325@users.noreply.github.com>
2021-11-21 23:28:38 +0900

    Build PATIENT_DB
    show PATIENT_DB except CRUD

--- server/router/user.js
+++ server/router/user.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { logout, see } from "../controller/user";
+import {logout, see, getEdit, postEdit} from "../controller/user";
 
 const userRouter = express.Router();
 
 
 userRouter.get("/logout", logout);
+userRouter.route("/edit-profile").get(getEdit).post(postEdit);
 // userRouter.get("/:id", see);
 
 export default userRouter;
