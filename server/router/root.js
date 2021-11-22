7de640d366ef3ae983b4a97c1052efa4712406e7
dbstn1325 <78465062+dbstn1325@users.noreply.github.com>
2021-11-22 16:11:16 +0900

    Build 'C' of CRUD

--- server/router/root.js
+++ server/router/root.js
@@ -1,12 +1,19 @@
 import express from "express";
 
-import { getHome, postHome, getJoin, postJoin, getLogin, postLogin } from "../controller/user"
+import { getHome, postHome, 
+    getJoin, postJoin, 
+    getLogin, postLogin,
+    getEnrollPatient, postEnrollPatient,
+    editPatient, deletePatient} from "../controller/user"
 
 const rootRouter = express.Router();
 
 rootRouter.route("/").get(getHome).post(postHome);
 rootRouter.route("/join").get(getJoin).post(postJoin);
 rootRouter.route("/login").get(getLogin).post(postLogin);
+rootRouter.route("/enroll").get(getEnrollPatient).post(postEnrollPatient);
+// rootRouter.post("/edit/:id", editPatient);
+// rootRouter.post("/delete/:id", deletePatient);
 
 
 export default rootRouter;
