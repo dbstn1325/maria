b8969b2d32dee39fe72543e727bb44bc9ad23b67
dbstn1325 <78465062+dbstn1325@users.noreply.github.com>
2021-12-01 23:59:22 +0900

    Check Backend error

--- server/router/user.js
+++ server/router/user.js
@@ -1,11 +1,9 @@
 import express from "express";
-import {logout, see, getEdit, postEdit} from "../controller/user";
+import {logout} from "../controller/user";
 
 const userRouter = express.Router();
 
 
 userRouter.get("/logout", logout);
-userRouter.route("/edit-profile").get(getEdit).post(postEdit);
-// userRouter.get("/:id", see);
 
 export default userRouter;
